d7fe2fd3cac4aa0b3c8fbf1af81fb2cf8f7cdd14
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-29 16:51:06 -0300

    Formulário lista produtos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaProdutosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lista_produtos';
+  protected readonly title = 'lista_produtos';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:ListaProdutosComponent
+    }
+];
