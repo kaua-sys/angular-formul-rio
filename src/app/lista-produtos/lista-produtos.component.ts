36331849fe2fb14e6522e52932b573949a41e654
Luiz Kauã <lkaua4546gmail.com>
2026-07-30 16:41:53 -0300

    Finalizado css por hoje, html adicioanado checkbox e ts adicioando função para limpar o array

--- src/app/lista-produtos/lista-produtos.component.ts
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Item } from './item';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -12,9 +13,25 @@ export class ListaProdutosComponent {
   id_produto: number = 0
   descricao_produto: string = ""
   valor_unitario: number = 0.0
+  listaItens: Item[] = []
 
   addItem (){
     console.log(this.descricao_produto,'<->', this.valor_unitario)
+    //Instanciando a classe Item
+    //let item = new Item(this.listaItens.length + 1,this.descricao_produto, this.valor_unitario)
+    let item = new Item()
+    item.idProduto = this.listaItens.length + 1
+    item.descricaoProduto = this.descricao_produto
+    item.valorUnitario = this.valor_unitario
+
+    this.listaItens.push(item)
+
+    this.descricao_produto = ""
+    this.valor_unitario = 0.0
   }
- 
+
+ limparTudo(): void {
+  this.listaItens = []
+ }
 }
+
