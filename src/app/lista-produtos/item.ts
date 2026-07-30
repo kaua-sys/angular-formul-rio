3d7907d899985b9bfe7f203647287ad1b58400e2
Luiz Kauã <lkaua4546gmail.com>
2026-07-30 16:40:41 -0300

    Criado arquivo item.ts

---
+++ src/app/lista-produtos/item.ts
@@ -0,0 +1,12 @@
+export class Item {
+    idProduto?: number
+    descricaoProduto?: string
+    valorUnitario?: number
+    statusSelecionado: boolean = false
+
+    /*constructor(idProduto: number,descricaoProduto: string,valorUnitario:number) {
+        this.id_produto = idProduto
+        this.descricaoProduto = descricaoProduto
+        this.valorUnitario = valorUnitario
+    }*/
+}
