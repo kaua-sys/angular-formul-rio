8324d54c4512955fcdca9ba6c6eddf7776f80b55
Luiz Kauã <lkaua4546gmail.com>
2026-07-30 15:32:54 -0300

    Finalizado css do formulário e criando arquivos ts

--- src/app/lista-produtos/lista-produtos.component.ts
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-produtos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.css'
 })
 export class ListaProdutosComponent {
+  id_produto: number = 0
+  descricao_produto: string = ""
+  valor_unitario: number = 0.0
 
+  addItem (){
+    console.log(this.descricao_produto,'<->', this.valor_unitario)
+  }
+ 
 }
